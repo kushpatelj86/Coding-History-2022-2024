7c2c4d67c62c6f70e416bc9ed507cc9cc56ea0f3
kushpatelj86 <111901629+kushpatelj86@users.noreply.github.com>
2023-02-07 12:45:18 -0800

    Add files via upload

---
+++ WordBlanks.js
@@ -0,0 +1,11 @@
+function wordBlanks(myNoun, myAdjective, myVerb,myAdverb)
+{
+    
+
+    var result = ""; 
+    result += "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb;
+
+
+    return result;
+}
+console.log(wordBlanks("dog","big","ran","quickly"))
